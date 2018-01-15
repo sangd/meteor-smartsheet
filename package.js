a47c0b13c9681f53fc7019aaaf074997ff1a99a2
Phuoc Do <dnprock@gmail.com>
2018-01-14 23:38:44 -0800

    Update version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Smartsheet OAuth flow",
-  version: "1.0.0",
+  version: "1.0.1",
   name: "sangd:meteor-smartsheet",
   git: "https://github.com/sangd/meteor-smartsheet.git"
 });
